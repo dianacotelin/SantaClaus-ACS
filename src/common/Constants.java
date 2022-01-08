d353b1b165aeed383bd1aaf437689aedab3075ee
dianacotelin <d_cotelin@yahoo.com>
2022-01-08 20:16:17 +0200

    Checker & checkstyle
    Add checker, checkstyle and enum files

---
+++ src/common/Constants.java
@@ -0,0 +1,45 @@
+package common;
+
+public final class Constants {
+    public static final String TEST_PATH = "tests/test";
+    public static final String OUTPUT_PATH = "output/out_";
+    public static final String REF_PATH = "ref/ref_test";
+    public static final String FILE_EXTENSION = ".json";
+    public static final Integer SMALL_TEST_POINTS = 2;
+    public static final Integer BIG_TEST_POINTS = 3;
+    public static final Integer TESTS_NUMBER = 25;
+    public static final Integer MAXIMUM_ERROR_CHECKSTYLE = 30;
+    public static final Integer CHECKSTYLE_POINTS = 10;
+    public static final Integer TESTS_NUMBER_SMALL = 15;
+    public static final String YEAR = "year";
+    public static final String NUMBER_OF_YEARS = "numberOfYears";
+    public static final String SANTA_BUDGET = "santaBudget";
+    public static final String INITIAL_DATA = "initialData";
+    public static final String CHILDREN = "children";
+    public static final String ID = "id";
+    public static final String LAST_NAME = "lastName";
+    public static final String FIRST_NAME = "firstName";
+    public static final String AGE = "age";
+    public static final String CITY = "city";
+    public static final String NICE_SCORE = "niceScore";
+    public static final String GIFT_PREFERENCE = "giftsPreferences";
+    public static final String SANTA_GIFT_LIST = "santaGiftsList";
+    public static final String PRODUCT_NAME = "productName";
+    public static final String PRICE = "price";
+    public static final String CATEGORY = "category";
+    public static final String ANNUAL_CHANGES = "annualChanges";
+    public static final String NEW_SANTA_BUDGET = "newSantaBudget";
+    public static final String NEW_GIFTS = "newGifts";
+    public static final String CHILDREN_UPDATES = "childrenUpdates";
+    public static final String NEW_CHILDREN = "newChildren";
+    public static final Integer BABY_SCORE = 10;
+    public static final Integer BABY_AGE = 5;
+    public static final Integer KID_AGE = 12;
+    public static final Integer TEEN_AGE = 18;
+    public static final Integer NO_TESTS = 26;
+
+    private Constants() {
+        //constructor for checkstyle
+    }
+
+}
